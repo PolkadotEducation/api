69a9dbfc065bb1093f17cc7d50ab439eb73e8903
Ewerton Onga <ewerton_onga@hotmail.com>
2024-10-20 13:16:13 -0300

    adding logo to email template

--- src/helpers/aws/templates/verificationLink.ts
+++ src/helpers/aws/templates/verificationLink.ts
@@ -10,7 +10,7 @@ export default `
     <table cellpadding="0" cellspacing="0" border="0" width="100%" style="max-width: 600px; margin: 0 auto; background-color: white; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
         <tr>
             <td style="padding: 20px; text-align: center;">
-                <h1 style="margin-top: 0;">Polkadot Education</h1>
+                <img src="https://app.polkadot.education/assets/images/logo.png" alt="Polkadot Education Logo" style="width: 166px; height: auto;">
                 <p>Thank you for registering. Please verify your email address to complete your registration.</p>
                 <a href="{{VERIFICATION_LINK}}" target="_blank" style="display: inline-block; background-color: #ff1493; color: white; padding: 10px 20px; border-radius: 20px; text-decoration: none; font-weight: bold;">Verify</a>
                 <p>If the button above doesn't work, please copy and paste the following URL into your browser:</p>
