e9e6f71a45ebf0190189ec56f321bf1bff2f9388
Lauro Gripa <laurogripa@gmail.com>
2025-07-27 22:45:45 -0300

    Drop database after tests

--- tests/db/setupTestMongo.ts
+++ tests/db/setupTestMongo.ts
@@ -5,6 +5,10 @@ let isMongoSettingUp = false;
 let isMongoReady = false;
 export const mongoDBsetup = async (db: string, disconnect: boolean = false) => {
   if (disconnect) {
+    if (mongoose.connection.db) {
+      await mongoose.connection.db.dropDatabase();
+    }
+
     await mongoose.disconnect();
     isMongoSettingUp = false;
     isMongoReady = false;
