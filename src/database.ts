4be57d3648225303521dca3f526664621c1cb564
Lauro Gripa <laurogripa@gmail.com>
2025-07-27 23:13:35 -0300

    Setup Sentry (#130)
    * Initial Sentry setup
    * Create test environment
    * Add mongo as a service to gh actions
    * Check mongo health
    * Set flags to false when disconnecting
    * Simplify test setup
    * Drop database after tests

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import { env } from "./environment";
 
-export const setupMongoDB = async (uri: string = env.MONGODB_URI) => {
+export const setupMongoDB = async (db: string) => {
   try {
+    const uri = `${env.MONGODB_URI}/${db}`;
     // eslint-disable-next-line no-console
     console.info(`MONGODB_URI: ${uri}`);
     await mongoose.connect(uri);
--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const NODE_ENV = process.env.NODE_ENV || "local";
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/doteducation";
 
